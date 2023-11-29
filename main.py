635e28fef1dc3a9a51dc4af7406cad24aa203679
MarcelRiedeman <141180911+dencmarcel@users.noreply.github.com>
2023-11-29 11:50:33 +0100

    geen erros meer

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
-from PIL import Image
 import numpy as np
 from scipy.fft import fft2, fftshift
+from PIL import Image
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
@@ -11,7 +11,7 @@ image_patch2_path = "images/patch F16-80.png"
 
 def resize_image(image_path, new_size=(120, 120)):
     image = Image.open(image_path).convert('L')  # Convert to grayscale
-    resized_image = image.resize(new_size, Image.ANTIALIAS)  # Use 'Image.ANTIALIAS' for older versions of Pillow
+    resized_image = image.resize(new_size, resample=Image.LANCZOS)
     return np.array(resized_image)
 
 
@@ -31,26 +31,43 @@ def image_to_dft(image_path, new_size=(120, 120)):
     return fourier_transform, magnitude_spectrum
 
 
-def depth_from_patch(DFT_patch1, DFT_patch2, D_1, D_2, f, u, v, s):
+def find_proper_u_v(DFT_patch1, DFT_patch2):
+    u, v = 1, 1
+    while u < 120:
+        while v < 120:
+            # Your logic here using DFT_patch1[u, v] and DFT_patch2[u, v]
+            if DFT_patch1[u, v] > 0 and DFT_patch2[u, v] > 0:
+                # Your code for the condition when DFT_patch1 and DFT_patch2 satisfy the condition
+                print(u, v)
+                print(DFT_patch1[u, v], DFT_patch2[u, v])
+                return u, v
+            v += 1
+        u += 1
+        v = 0  # Reset v for the next iteration of the outer loop
+
+
+def depth_from_patch(DFT_patch1, DFT_patch2, D_1, D_2, f, s):
+    u, v = find_proper_u_v(DFT_patch1, DFT_patch2)
+
     # Ensure the logarithms are computed for non-negative values
-    log_DFT_patch1 = np.log(np.maximum(DFT_patch1, 1e-10))
-    log_DFT_patch2 = np.log(np.maximum(DFT_patch2, 1e-10))
+    log_DFT_patch1 = np.log(DFT_patch1[u, v])
+    log_DFT_patch2 = np.log(DFT_patch2[u, v])
 
     sigma_2 = math.sqrt(log_DFT_patch2 - log_DFT_patch1) / (
-                2 * np.pi ** 2 * (u ** 2 + v ** 2) * ((D_1 ** 2 / D_2 ** 2) - 1))
+            2 * np.pi ** 2 * (u ** 2 + v ** 2) * ((D_1 ** 2 / D_2 ** 2) - 1))
     depth = s * f / (s - f + 2 * sigma_2 * (f / D_2))
 
     return depth
 
 
-def plot_image_and_dft(image_path):
+def plot_image_and_dft(image_path, new_size=(120, 120)):
     # Get DFT and magnitude spectrum
-    dft, magnitude_spectrum = image_to_dft(image_path)
+    dft, magnitude_spectrum = image_to_dft(image_path, new_size)
 
     # Display the original image and its Fourier Transform
     plt.figure(figsize=(12, 6))
 
-    plt.subplot(121), plt.imshow(Image.open(image_path), cmap='gray')
+    plt.subplot(121), plt.imshow(resize_image(image_path, new_size), cmap='gray')
     plt.title('Original Image'), plt.axis('off')
 
     plt.subplot(122), plt.imshow(np.log(1 + magnitude_spectrum), cmap='gray', norm=LogNorm())
@@ -62,7 +79,7 @@ def plot_image_and_dft(image_path):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     plot_image_and_dft(image_patch1_path)
-    # Depth Calculation
+    # Example usage
     new_size = (120, 120)
     dft_patch1, _ = image_to_dft(image_patch1_path, new_size)
     dft_patch2, _ = image_to_dft(image_patch2_path, new_size)
@@ -71,8 +88,7 @@ if __name__ == '__main__':
     D_1 = 1.0  # Replace with actual values
     D_2 = 2.0  # Replace with actual values
     f = 50.0  # Replace with actual values
-    u, v = 0.0, 0.0  # Replace with actual values
     s = 100.0  # Replace with actual values
 
-    calculated_depth = depth_from_patch(dft_patch1, dft_patch2, D_1, D_2, f, u, v, s)
+    calculated_depth = depth_from_patch(dft_patch1, dft_patch2, D_1, D_2, f, s)
     print(f"Calculated depth: {calculated_depth}")
